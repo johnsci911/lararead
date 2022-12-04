06d65e2733d84c54e4e8c0523c8021dc55578314
johnsci911 <johnguitarizta@gmail.com>
2022-12-04 23:37:08 +0800

    Creating ideas with validations

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -18,7 +18,8 @@ class IdeaController extends Controller
         return view('idea.index',
             [
                 'ideas' => Idea::with('user', 'category', 'status')
-                    ->simplePaginate(Idea::PAGINATION_COUNT)
+                    ->orderBy('id', 'desc')
+                    ->simplePaginate(Idea::PAGINATION_COUNT),
             ]
         );
     }
